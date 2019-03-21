06bb576376a5553e6e24d0d1396baba61a89520d
Stephen R. Foster <stephen@thoughtstem.com>
2019-03-21 15:04:50 -0700

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,6 @@
 #lang info
 (define collection "c")
-(define deps '("base"
-               "https://github.com/thoughtstem/racket-pkg-test-d.git"))
+(define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/c.scrbl" ())))
 (define pkg-desc "Description Here")
